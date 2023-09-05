eb5dfb6a2315177bea0d3785b11fed5a2c2210a5
D Rao <tdayakarrao@gmail.com>
2023-09-05 17:13:20 +0530

    Pattern Programs

--- PatternPrograms/src/letters/SqureBoxStars.java
+++ PatternPrograms/src/letters/SqureBoxStars.java
@@ -4,7 +4,7 @@ public class SqureBoxStars {
 
 	public static void main(String[] args) {
 		
-		int n = 10;
+		int n = 12;
 		
 		for(int i = 0; i < n; i++) {
 			
@@ -139,14 +139,31 @@ public class SqureBoxStars {
 					System.out.print(" ");	
 			
 				}
+				
+				System.out.print("   ");
+				
+				
+				
+				for(j = 0; j < n; j++) {
+					
+					if(i == 0 ||  i == n-1 || j == 0 ||  j == (n-1)/4 || j == (n-1)/2 || j == (3 * n)/4 || j == (n-1)/2 || j == n-1) {
+					
+						System.out.print("*");
+				
+					}
+					else {				
+				
+						System.out.print(" ");	
+				
+					}
+				
+				}//j loop
 			
 			}//j loop
 			
 			System.out.println();
 			
-		}// i loop
-		
-		//System.out.println();
+		}// i loop		
 
 	}// main
 
