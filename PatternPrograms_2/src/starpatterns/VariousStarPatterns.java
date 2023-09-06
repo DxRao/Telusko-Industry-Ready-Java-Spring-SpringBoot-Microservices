de94beef19f30d59310a83fb80ad4cda76e291ad
D Rao <tdayakarrao@gmail.com>
2023-09-07 02:15:32 +0530

    Pattern Programs-2

--- PatternPrograms_2/src/starpatterns/VariousStarPatterns.java
+++ PatternPrograms_2/src/starpatterns/VariousStarPatterns.java
@@ -4,14 +4,50 @@ public class VariousStarPatterns {
 
 	public static void main(String[] args) {		
 
-		int n = 23;	
+		int n = 17;	
 
 		for(int i = 0; i < n; i++) {			
-
-			for(int j = 0; j < n; j++) {
-
+			
+			for(int j = 0; j <n; j++) {
+			
 				if(i == j ||  i+j == n-1 || i+j == (n-1)/2 ||  i - j == (n-1)/2 
 				|| j - i == (n-1)/2 || i+j == (n-1)+(n-1)/2 || i == 0 || j ==0 || i == n-1 || j == n-1) {
+
+					System.out.print("*");
+							
+				}
+						
+				else{				
+
+					System.out.print(" ");	
+
+				}
+
+			}//j loop
+			
+			System.out.print("    ");
+			
+			for(int j = 0; j < n; j++) {
+
+				if( i+j == (n-1)/2 ||  i - j == (n-1)/2 || j ==0 ) {
+
+					System.out.print("*");
+						
+				}
+					
+				else{				
+
+					System.out.print(" ");	
+
+				}
+
+			}//j loop	
+			
+			System.out.print("");
+			
+			for(int j = 0; j < n; j++) {
+
+				if( i == 0 || i == n-1 || i + j == n-1 ) {
 
 					System.out.print("*");
 						
@@ -32,9 +68,3 @@ public class VariousStarPatterns {
 	}// main
 
 }// class
-
-		
-
-	
-
-
