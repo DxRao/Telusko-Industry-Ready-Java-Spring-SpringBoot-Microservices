ae002f78174cca3491582b8dff1764d49328c91e
D Rao <tdayakarrao@gmail.com>
2023-09-07 19:27:44 +0530

    Pattern Programs-2

--- PatternPrograms_2/src/starpatterns/VariousStarPatterns.java
+++ PatternPrograms_2/src/starpatterns/VariousStarPatterns.java
@@ -43,11 +43,48 @@ public class VariousStarPatterns {
 
 			}//j loop	
 			
-			System.out.print("");
+			System.out.print(""); 
 			
 			for(int j = 0; j < n; j++) {
 
 				if( i == 0 || i == n-1 || i + j == n-1 ) {
+
+					System.out.print("*");
+						
+				}
+					
+				else{				
+
+					System.out.print(" ");	
+
+				}
+
+			}//j loop
+			
+			System.out.print("  "); 
+			
+			for(int j = 0; j < n; j++) {
+
+				if( j == 0 || i == j && (i < (n-1)/2) || i+j == n-1 && i < (n-1)/2 || j == n-1 ) {
+
+					System.out.print("*");
+						
+				}
+					
+				else{				
+
+					System.out.print(" ");	
+
+				}
+
+			}//j loop
+			
+			System.out.print("  "); 
+			
+			for(int j = 0; j < n; j++) {
+
+				if( j == 0 && (i > 0 && i < (3*n)/4) || i == 0 && (j > 0 && j < (3*n)/4) || i == (3 *n)/4 && (j > 0 && j < (3*n)/4) 
+						|| j == (3 * n)/4 && (i > 0 && i < (3*n)/4) || i == j && i > (n-1)/2) {
 
 					System.out.print("*");
 						
