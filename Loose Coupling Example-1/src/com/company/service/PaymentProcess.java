cdbf28d7243d08afdac0162944f9a2c6d9adb715
D Rao <tdayakarrao@gmail.com>
2023-10-04 19:17:15 +0530

    Loose Coupling Example-1

--- Loose Coupling Example-1/src/com/company/service/PaymentProcess.java
+++ Loose Coupling Example-1/src/com/company/service/PaymentProcess.java
@@ -4,33 +4,39 @@ public class PaymentProcess {
 	
 	IPay pay;
 	
+	// no parameterized default constructor
 	public PaymentProcess() {
+		
 		super();
 	}
 	
 	// parameterized constructor injection
 	public PaymentProcess(IPay pay) {
+		
 		super();
+		
 		this.pay = pay;
 	} 
 
 	// setter injection
 	public void setPay(IPay pay) {
+		
 		this.pay = pay;
 	}
 	
-	public boolean doPayment(double amount)
-	{
+	public boolean doPayment(double amount){
+	
 		boolean status = pay.payBill(amount);
 		
-		if(status)
-		{
-			System.out.println("Payment success");
+		if(status){
+		
+			System.out.println("Payment successful. Thank you!");
 		}
-		else
-		{
-			System.out.println("Payment Failed");
+		else{
+		
+			System.out.println("Payment Failed. Sorry try again.");
 		}
-		return true;
+		
+		return status;
 	}
 }
