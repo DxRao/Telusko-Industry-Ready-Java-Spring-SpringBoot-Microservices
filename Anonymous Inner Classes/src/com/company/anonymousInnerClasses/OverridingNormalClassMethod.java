5c2d66455c2a7bddd3d063007bb366e1c1c554a4
D Rao <tdayakarrao@gmail.com>
2023-10-07 21:40:20 +0530

    Overriding Normal method of Parent class in Anonymous Inner Class

--- Anonymous Inner Classes/src/com/company/anonymousInnerClasses/OverridingNormalClassMethod.java
+++ Anonymous Inner Classes/src/com/company/anonymousInnerClasses/OverridingNormalClassMethod.java
@@ -11,6 +11,10 @@ class P{
 		
 		System.out.println("in class P show1()");
 	}
+	
+	void abc() {
+		System.out.println("in abc() method");
+	}
 }
 	
 
@@ -19,7 +23,7 @@ class P{
  * println("Purpose of class Q is to override the P class show() method"); } }
  */
 
-public class OverridingNormalClassMethod {
+public class OverridingNormalClassMethod { // Anonymous inner class of no name belongs to this outer class
 
 	public static void main(String[] args) {
 		
@@ -41,10 +45,13 @@ public class OverridingNormalClassMethod {
 			void show2() { // you can have a local method but you can't call or use it(only a warning from compiler)
 				System.out.println(" In show2() method");
 			}
-		};		
+		}; 
+		// dot class file is generated for Anonymous inner class ( outerClassName$01.class)		
+		//dot class file is not generated for lamda expression though.
 		
 		obj.show();	
 		obj.show1();
-		//obj.show2(); // Compile Time error: The method show2() is undefined for type P		
+		//obj.show2(); // Compile Time error: The method show2() is undefined for type P
+		obj.abc(); // this method call for abc() method of parent class P is allowed even though its not overridden.
 	}
 }
