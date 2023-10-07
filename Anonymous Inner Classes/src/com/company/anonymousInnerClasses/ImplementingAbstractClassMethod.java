c7ed99eff18c2447af20c7dd9a3bfa596b4591ef
D Rao <tdayakarrao@gmail.com>
2023-10-07 22:13:37 +0530

    Anonymous Inner Class

--- Anonymous Inner Classes/src/com/company/anonymousInnerClasses/ImplementingAbstractClassMethod.java
+++ Anonymous Inner Classes/src/com/company/anonymousInnerClasses/ImplementingAbstractClassMethod.java
@@ -1,10 +1,33 @@
 package com.company.anonymousInnerClasses;
 
+abstract class ABC{
+	void show1() {
+		
+		System.out.println("in show1() method");
+	}
+	
+	abstract void show2();	
+}
+
 public class ImplementingAbstractClassMethod {
 
 	public static void main(String[] args) {
 		
-
+		ABC abc = new ABC() { // Implementation of show2() method of abstract class ABC thru Anonymous inner class
+			
+			void show2() {
+				
+				System.out.println("in show2() method");
+			}
+			
+		};
+		
+		abc.show1();
+		abc.show2();
 	}
-
 }
+
+/*
+ * in show1() method 
+ * in show2() method
+ */
--- Anonymous Inner Classes/src/com/company/anonymousInnerClasses/ImplementingInterfaceAbstractMethod.java
+++ Anonymous Inner Classes/src/com/company/anonymousInnerClasses/ImplementingInterfaceAbstractMethod.java
@@ -1,10 +1,36 @@
 package com.company.anonymousInnerClasses;
 
+interface XYZ{
+	
+	void show1(); 
+	void show2();	
+}
+
 public class ImplementingInterfaceAbstractMethod {
 
 	public static void main(String[] args) {
 		
-
+		XYZ xyz = new XYZ() { // Implementation of show2() method of abstract class ABC thru Anonymous inner class
+			
+			public void show1() { // public is a must for implementing public abstract method of interface
+				
+				System.out.println("in show1() method");
+			}
+			
+			public void show2() { // public is a must for implementing public abstract method of interface
+				
+				System.out.println("in show2() method");
+			}
+			
+		};
+		
+		xyz.show1();
+		
+		xyz.show2();
 	}
-
 }
+
+/*
+ * in show1() method 
+ * in show2() method
+ */
