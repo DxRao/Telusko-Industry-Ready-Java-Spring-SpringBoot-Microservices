e7e082f17a055c433906062fda37fc886d605a34
D Rao <tdayakarrao@gmail.com>
2023-10-15 18:18:01 +0530

    String Basics

---
+++ String in Java/src/com/company/string/StringBasics.java
@@ -0,0 +1,26 @@
+package com.company.string;
+
+public class StringBasics {
+
+	public static void main(String[] args) {
+		
+		String s1=new String("Java");
+		String s2=new String("Java");
+		
+		System.out.println(s1==s2);//false
+		
+		System.out.println(s1.equals(s2));//true
+		
+		
+		String str1="Java";
+		String str2="Java";
+		
+		System.out.println(str1==str2);// true
+		
+//		s1=s1.concat("Spring");
+//		
+//		System.out.println(s1);
+
+	}
+
+}
