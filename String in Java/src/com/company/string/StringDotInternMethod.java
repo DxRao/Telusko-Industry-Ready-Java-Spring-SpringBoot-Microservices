815d6c1a004fa31ebf8e030e0030a446397e91e2
D Rao <tdayakarrao@gmail.com>
2023-10-15 18:11:44 +0530

    String in Java

---
+++ String in Java/src/com/company/string/StringDotInternMethod.java
@@ -0,0 +1,17 @@
+package com.company.string;
+
+public class StringDotInternMethod {
+
+	public static void main(String[] args) {
+		
+		String s1=new String("Java");
+		
+		String s2=s1.intern();
+		
+		String s3="Java";
+		
+		System.out.println(s2==s3);
+
+	}
+
+}
