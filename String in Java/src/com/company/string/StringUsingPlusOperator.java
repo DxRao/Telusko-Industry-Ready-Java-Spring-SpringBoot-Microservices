ba8ba5d5d886a8a995e56165c4f43bfd626d1fc9
D Rao <tdayakarrao@gmail.com>
2023-10-15 18:25:09 +0530

    String Using Plus Operator

---
+++ String in Java/src/com/company/string/StringUsingPlusOperator.java
@@ -0,0 +1,25 @@
+package com.company.string;
+
+public class StringUsingPlusOperator {
+
+	public static void main(String[] args) {
+		
+		String str1="Java";
+		
+		String str2="Ja";
+		String str3="va";
+		
+		
+		String str4=str2+str3;//java ... Using plus operator with reference variables
+		
+		System.out.println(str1==str4);//false
+		
+		String str5="Hyder";
+		String str6="Hy"+"der"; // Using plus operator with String literals
+		
+		System.out.println(str5==str6);//true
+		
+
+	}
+
+}
