8c7ecd315946a9bdc971bed2102db78f156cdba5
D Rao <tdayakarrao@gmail.com>
2023-11-12 00:00:04 +0530

    Unit testing

---
+++ JUnit-2/src/com/company/unitTesting/Calc1.java
@@ -0,0 +1,10 @@
+package com.company.unitTesting;
+
+public class Calc1 {
+	public int add(int a, int b) {
+		
+		return a + b;
+		
+	}
+
+}
---
+++ JUnit-2/test/com/company/unitTesting/Calc1Test.java
@@ -0,0 +1,14 @@
+package com.company.unitTesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calc1Test {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
