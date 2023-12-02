e2f5c88fedfbfd5cc1773f83b56cc642455f4ff7
D Rao <tdayakarrao@gmail.com>
2023-12-02 18:40:08 +0530

    CRUD Operations Using Prepared Statement

--- MySQL - JDBC - CRUD - By Prepared statement/src/com/company/jdbc/main/DeleteData.java
+++ MySQL - JDBC - CRUD - By Prepared statement/src/com/company/jdbc/main/DeleteData.java
@@ -2,7 +2,71 @@ package com.company.jdbc.main;
 
 import com.company.jdbc.util.JdbcUtility;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
-// DeleteData 
+public class DeleteData{ 
 
+	public static void main(String[] args){
+		
+		//Resources
+				Connection connect=null;
+				PreparedStatement stmt=null;
+				ResultSet rs=null;
+				Scanner scan=null;
+				
+				try
+				{					
+					connect=JdbcUtility.getDBConnection();
+					
+					if(connect!=null)
+					{
+						stmt=connect.prepareStatement("DELETE FROM Employee where id=?");
+					}
+					
+
+					if(stmt!=null)
+					{
+						System.out.println("Welcome to my world!");
+						
+						scan=new Scanner(System.in);
+						System.out.println("Please enter your id");
+						Integer id=scan.nextInt();
+						
+						stmt.setInt(1, id);
+						
+						
+						int rowAffected=stmt.executeUpdate();
+						
+						if(rowAffected != 0)
+							System.out.println("Delete successfull");
+						else
+							System.out.println("Failed to delete!");						
+					}						
+				}
+				catch(SQLException e)
+				{
+					e.printStackTrace();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				finally
+				{
+					try 
+					{
+						JdbcUtility.closeResource(connect, stmt, rs);
+					} 
+					catch (SQLException e) 
+					{						
+						e.printStackTrace();
+					}
+				}
+	}
+}
 
