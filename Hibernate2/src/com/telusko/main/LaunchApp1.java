6aeb7d8d293b0c4dcc9eb9abbd211234191d2c57
D Rao <tdayakarrao@gmail.com>
2023-12-13 01:03:04 +0530

    Using ORM tool Hibernate to connect and perform CRUD operations.

---
+++ Hibernate2/src/com/telusko/main/LaunchApp1.java
@@ -0,0 +1,45 @@
+package com.telusko.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.telusko.entity.Student;
+
+public class LaunchApp1 {
+
+	public static void main(String[] args) 
+	{
+		//Inform JVM to activate Hibernate
+		Configuration config = new Configuration();
+		config.configure();
+		
+		//Creating SessionFactory to hold all other required objects for hibernate
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        
+        //Persistence operation
+       Session session = sessionFactory.openSession();
+       //Only for non select operation transaction must be there
+       Transaction transaction = session.beginTransaction();
+       
+       //Create Persistence Object
+       Student st=new Student();
+       st.setId(2);
+       st.setName("Sharmik");
+       st.setAge(22);
+       st.setCity("Pune");
+       
+       //Perform persistence operation
+       //session.update(st);
+       session.saveOrUpdate(st);
+       
+       //commit the operation
+       transaction.commit();
+       
+       session.close();
+       sessionFactory.close();      
+       
+	}
+
+}
