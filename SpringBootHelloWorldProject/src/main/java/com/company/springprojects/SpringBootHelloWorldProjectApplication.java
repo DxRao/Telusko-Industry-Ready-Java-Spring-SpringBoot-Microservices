ce9cd409daf1020866893220b5e1519d72cdfe2f
D Rao <tdayakarrao@gmail.com>
2023-12-23 01:14:33 +0530

    Spring Boot Hello World Project

--- SpringBootHelloWorldProject/src/main/java/com/company/springprojects/SpringBootHelloWorldProjectApplication.java
+++ SpringBootHelloWorldProject/src/main/java/com/company/springprojects/SpringBootHelloWorldProjectApplication.java
@@ -2,13 +2,18 @@ package com.company.springprojects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class SpringBootHelloWorldProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldProjectApplication.class, args);
-		System.out.println("Hello World Spring Boot");
+		
+		System.out.println("Hello World And Welcome To The World Of Spring Boot");
+		ApplicationContext context = SpringApplication.run(SpringBootHelloWorldProjectApplication.class, args);
+		Student_100_Years student = context.getBean(Student_100_Years.class);
+		student.learning();
 	}
 
 }
---
+++ SpringBootHelloWorldProject/src/main/java/com/company/springprojects/Student_100_Years.java
@@ -0,0 +1,19 @@
+package com.company.springprojects;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student_100_Years {
+	
+	public Student_100_Years() {
+		
+		System.out.println("Student obj created");
+	}
+	
+	public void learning() {
+		
+		System.out.println("Learning is a life long process and you are a Student forever and that too for a lifetime.");
+		
+	}
+
+}
