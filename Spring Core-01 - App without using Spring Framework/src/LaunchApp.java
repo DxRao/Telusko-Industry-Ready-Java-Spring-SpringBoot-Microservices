09c667356843c183cefd29fe5a50694b2b81b804
D Rao <tdayakarrao@gmail.com>
2024-04-22 18:22:43 +0530

    Spring Core-01 (Setter Injection) without using Spring Framework

--- Spring Core-01 - App without using Spring Framework/src/LaunchApp.java
+++ Spring Core-01 - App without using Spring Framework/src/LaunchApp.java
@@ -1,21 +1,23 @@
 
-public class LaunchApp 
-{
+public class LaunchApp {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args){ 
+	
 		GreetingsGenerator greet=new GreetingsGenerator();
-		System.out.println("Target Object before injecting Dependent Object => "+ greet);
+		
+		System.out.println("Target Object before injecting Dependent Object => " + greet);
 		
 		java.util.Date date=new java.util.Date();
-		System.out.println("Dependent Obj =>"+ date);
+		
+		System.out.println("Dependent Obj => " + date);
 		
 		greet.setDate(date);//Setter Injection
 		
 		
 		System.out.println("Target Object After injecting Dependent Object => "+ greet);
 			
-		String message=greet.generateGreeting("Rao");
+		String message=greet.generateGreeting("D Rao");
+		
 		System.out.println(message);
 	}
 
